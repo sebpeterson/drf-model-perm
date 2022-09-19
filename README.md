4cdbf1b963dfa3cd233669f05b02d75fe96afb59
Seb Peterson <sebastien.peterson@gmail.com>
2022-09-19 14:58:20 +0200

    init

--- README.md
+++ README.md
@@ -46,3 +46,29 @@ class ToolViewSet(viewsets.ModelViewSet):
     queryset = Tool.objects.all()
     serializer_class = ToolSerializer
 ```
+
+
+RESOLVED BY
+looking at https://stackoverflow.com/questions/46584653 django-rest-framework-use-djangomodelpermissions-on-listapiview
+
+and 
+
+views.py
+```
+class CustomDjangoModelPermission(DjangoModelPermissions):
+    """"
+    https://stackoverflow.com/questions/46584653/django-rest-framework-use-djangomodelpermissions-on-listapiview
+    """
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+class ToolViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    permission_classes = [CustomDjangoModelPermission]
+    
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
+```
--- drf_model_perm_app/views.py
+++ drf_model_perm_app/views.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.shortcuts import render
 from drf_model_perm_app.models import Tool
 from drf_model_perm_app.serializers import ToolSerializer
@@ -6,11 +8,20 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import DjangoModelPermissions
 
 # Create your views here.
+
+class CustomDjangoModelPermission(DjangoModelPermissions):
+    """"
+    https://stackoverflow.com/questions/46584653/django-rest-framework-use-djangomodelpermissions-on-listapiview
+    """
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
 class ToolViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomDjangoModelPermission]
     
     queryset = Tool.objects.all()
     serializer_class = ToolSerializer
